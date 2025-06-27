75095a908dbce01f1131956cc414aca2185b2345
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-27 12:28:02 +0530

    Removed comments, Added new fields : img, goals, and changed field name of tech interest to tech stacks.

--- model/model.js
+++ model/model.js
@@ -3,59 +3,37 @@ const validator = require("validator")
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-// schema validations:
-/**?
- * -> "type": type of the field
- * -> "required" : mandatory or not
- * -> "unique" : gives unique identity, meaning no same data again
- * -> "default" : set default values, if not entered value, then that value will push to database
- * -> "lowercase", "uppercase", "trim" : for string type fields
- * -> "minLength", "maxLength" : minimum or maximum length of the string field
- * -> "min", "max" : minimum or maximum validation for number and date field
- * 
- * -> type, require, unique, default, min, max, minLength, maxLength, lowercase, uppercase, trim, match
- */
-
-
-/**
- * -> some predefined vaidations allow array values where first value is the value of that validation and second value is message that will show when the field value is not validate.
- * -> required, min, max, minLength, maxLength, match, unique can have array access.
- */
-
-// Timestamps is the second parameter that allows you to be aware of when the field is created and when it is updated.
 
 const userSchema = new mongoose.Schema({
+    img: {
+        type: String,
+        required: [true, "Profile Picture is required"]
+    },
+
     firstName: {
         type: String,
         required: [true, "First name is required"],
         minLength: [4, "First Name must not be less than 4 characters"],
         maxLength: [50, "First Name must not be greater than 50 characters"]
     },
+
     lastName: {
         type: String,
         required: [true, "Last name is required"],
         minLength: [4, "Last Name must not be less than 4 characters"],
         maxLength: [50, "Last Name must not be greater than 50 characters"]
     },
+
     email: {
-
-        // unique gives power to not accept two same email ids, if one is registred, then that one cannot register again.
-        // without trim() mongo will assume two same email ids are different, just because one has trailing spaces and one has not.
-        // therefore , whenever you define unique, define trim too with strings.
-
         type: String,
         required: [true, "Email is required"],
         unique: [true, `this email is already registered`],
-        sparse:true,
+        sparse: true,
         lowercase: true,
         trim: true,
         match: [/.+\@.+\..+/, `{VALUE} is invalid`]
-        // validate(value) {
-        //     if(!validator.isEmail(value)) {
-        //         throw new Error(`Email id is invalid : ${value}`)
-        //     }
-        // }
     },
+
     passWord: {
         type: String,
         required: [true, "Password is required"],
@@ -65,15 +43,18 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+
     state: {
         type: String,
         required: [true, "State is required"]
     },
+
     age: {
         type: Number,
         required: [true, "Age is required"],
         min: [0, "age must be greater than or equals to 0"]
     },
+
     gender: {
         type: String,
         required: [true, "Gender is required"],
@@ -81,45 +62,48 @@ const userSchema = new mongoose.Schema({
             values: ['male', 'female', 'others'],
             message: `{VALUE} is invalid`
         }
-
-        // custom validation
-        // validate(value) {
-        //     if (!["male", "female", "others"].includes(value)) {
-        //         throw new Error("User data is invalid")
-        //     }
-        // }
     },
+
     occupation: {
         type: String,
         required: [true, "Occupation is required"]
     },
-    techInterests: {
+
+    techStacks: {
         type: [String],
-        validate : {
-            validator : function(arr) {
-                return Array.isArray(arr) && arr.length>0
+        validate: {
+            validator: function (arr) {
+                return Array.isArray(arr) && arr.length > 0
             },
-            message: "At least one tech interest is required"
+            message: "At least one techStacks is required"
         },
-        required: [true, "Tech Interest is required. e.g. Programming languages, Hackathons"]
+        required: [true, "Tech Stack is required. e.g. Programming languages, Hackathons"]
     },
-    isMarried: {
-        type: Boolean,
-        required: false
+    goals: {
+        type: [String],
+        validate: {
+            validator: function (arr) {
+                return Array.isArray(arr) && arr.length > 0
+            },
+            message: "At least one Goal is required"
+        },
+        required: [true, "Goal is required. e.g. Networoking, Tech talks"]
     }
+
 }, { timestamps: true })
 
 
 userSchema.index({ firstName: 1, lastName: 1 });
 
-
+// generate JWT token as id which will hide is the part of userschema.
 userSchema.methods.genJWT = async function () {
     const id = this._id;
-    const token = await jwt.sign({ _id: id }, "SECRET@VERYsecret123", { expiresIn: "1h" })
+    const token = await jwt.sign({ _id: id }, process.env.JWT_SECRETKEY, { expiresIn: "1h" })
 
     return token;
 }
 
+// verify password as we compare password which is the part of userschema
 userSchema.methods.verifyPassword = async function (userPassword) {
     const pw = this.passWord;
     const passwordCheck = await bcrypt.compare(userPassword, pw);
