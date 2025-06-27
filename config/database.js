2b6c96a010c202c36c6451ca2325372316230ad6
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-27 12:20:09 +0530

    Chnaged raw mongodb connection url to secure MONGO_URL of .env variables.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    await mongoose.connect("mongodb+srv://rinkal159:dhHRFf2YR1XCizbq@newkiddo.9rtpo7p.mongodb.net/devTinder");
+    await mongoose.connect(process.env.MONGO_URL);
 }
 
 module.exports = connect;
