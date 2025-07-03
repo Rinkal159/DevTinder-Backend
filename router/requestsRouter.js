d48087d112dfb2ff8ba23f09a880a64462e3c191
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-03 19:24:32 +0530

    Made changes in request routes, now receiving senderID from params not reuqestID

--- router/requestsRouter.js
+++ router/requestsRouter.js
@@ -19,8 +19,6 @@ requestsRouter.use(cookieParser())
             const receiverID = req.params.receiverID;
             const requestStatus = req.params.requestStatus;
 
-            console.log(requestStatus);
-
 
 
             // sender profile
@@ -52,28 +50,9 @@ requestsRouter.use(cookieParser())
                     }
 
                 }
-                else if (requestStatus === "ignored" && existingRequestOfSender.requestStatus === "interested") {
-                    await ConnectionRequest.updateOne({ _id: existingRequestOfSender.id }, { requestStatus: requestStatus })
-
-                    return res.json({
-                        message: `${sender.firstName.toUpperCase()}, you have successfully changed the request status from ${existingRequestOfSender.requestStatus.toUpperCase()} to ${requestStatus.toUpperCase()}`
-
-                    })
-                }
 
             }
 
-            // check existing request of receiver to sender who is trying to send request to that receiver
-
-            /** PENDING NEW FEATURE */
-            const ExistingRequestOfReceiverTosender = await ConnectionRequest.findOne({ senderID: receiverID, receiverID: senderID })
-
-            if (ExistingRequestOfReceiverTosender) {
-
-                if (ExistingRequestOfReceiverTosender.requestStatus === "interested") {
-                    throw new Error(`${sender.firstName.toUpperCase()}, You have already received connection request from ${receiver.firstName.toUpperCase()}`)
-                }
-            }
 
 
             const requestData = new ConnectionRequest({
@@ -83,10 +62,7 @@ requestsRouter.use(cookieParser())
             })
 
             await requestData.save();
-            res.json({
-                // message: `${sender.firstName.toUpperCase()}, You have successfully SENT Connection request to ${receiver.firstName.toUpperCase()} !`,
-                data: receiver
-            })
+            res.json(receiver)
 
 
 
@@ -100,35 +76,32 @@ requestsRouter.use(cookieParser())
     // POST /request/review/:requestStatus/:requestID
     // accepted or rejected
 
-    requestsRouter.post("/request/review/:requestStatus/:requestID", userAuth, async (req, res) => {
+    requestsRouter.post("/request/review/:requestStatus/:senderID", userAuth, async (req, res) => {
         try {
-            const loggedInUserID = req.id;
-            const { requestStatus, requestID } = req.params;
+            const id = req.id;
+            const { requestStatus, senderID } = req.params;
 
             const loggedInUser = req.user;
 
-            const requestProfile = await ConnectionRequest.findOne({ _id: requestID, requestStatus: "interested" });
+            const requestSender = await User.findById(senderID);
+
+            const requestProfile = await ConnectionRequest.findOne({ senderID: senderID, receiverID: id })
 
             // to check the existance of request id and if it exists, check whether the requets is meant to send for loggedInUser.
-            if (!requestProfile) {
-                throw new Error(`Request not found`)
+            if (!requestSender) {
+                throw new Error(`User not found`)
 
-            } else if (requestProfile.receiverID.toString() !== loggedInUserID) {
-                throw new Error(`This request is not for you!`)
             }
 
-            const RequestSenderID = requestProfile.senderID;
-            const requestSender = await User.findById(RequestSenderID);
-
             // to now allow outsider status
-            const allowedStatus = ["accepted", "rejected"];
+            const allowedStatus = ["Accept", "Reject"];
             if (!allowedStatus.includes(requestStatus)) {
                 throw new Error(`Request status : ${requestStatus} is invalid`)
             }
 
             // check whether you have already accepted or rejected the request and again you trying to do same
             if (requestProfile.requestStatus === requestStatus) {
-                if (requestStatus === "accepted") {
+                if (requestStatus === "Accept") {
                     throw new Error(`${loggedInUser.firstName.toUpperCase()}, You have already successfully ACCEPTED connection request of ${requestSender.firstName.toUpperCase()}`)
 
                 } else {
@@ -136,23 +109,13 @@ requestsRouter.use(cookieParser())
                 }
             }
 
-            await ConnectionRequest.updateOne({ _id: requestID }, { requestStatus: requestStatus }, { runValidators: true })
+            await ConnectionRequest.updateOne({ _id: requestProfile._id }, { requestStatus: requestStatus }, { runValidators: true })
 
 
 
             //Response
-            if (requestStatus === "accepted") {
 
-                res.json({
-                    message: `${loggedInUser.firstName.toUpperCase()}, You have successfully ACCEPTED connection request of ${requestSender.firstName.toUpperCase()}`
-                })
-            }
-            else {
-
-                res.json({
-                    message: `${loggedInUser.firstName.toUpperCase()}, You have successfully REJECTED connection request of ${requestSender.firstName.toUpperCase()}`
-                })
-            }
+            res.json(requestSender)
 
 
 
@@ -167,29 +130,24 @@ requestsRouter.use(cookieParser())
 {
     // DELETE /request/delete
 
-    requestsRouter.delete("/request/delete/:requestID", userAuth, async (req, res) => {
+    requestsRouter.delete("/request/:status/:receiverID", userAuth, async (req, res) => {
         try {
             const id = req.id;
-            const user = req.user;
-            const requestID = req.params.requestID;
+            const receiverID = req.params.receiverID;
+            const status = req.params.status;
 
-            const deletedID = await ConnectionRequest.findOne(
-                {
-                    $and:
-                        [{ $and: [{ _id: requestID }, { senderID: id }] }, { requestStatus: "interested" }]
+            const receiver = await User.findById(receiverID);
+            if (!receiver) {
+                res.status(401).json({
+                    success: 'failed',
+                    message: "user not found!"
                 })
-                .populate("receiverID", "firstName lastName")
-
-            if (!deletedID) {
-                throw new Error("Request not found")
             }
 
-            await ConnectionRequest.deleteOne({ _id: deletedID });
 
-            res.json({
-                message: `${user.firstName}, You have successfully DELETED your connection request`,
-                deletedDevProfile: deletedID.receiverID
-            })
+            await ConnectionRequest.updateOne({ senderID: id, receiverID: receiverID }, { requestStatus: status });
+
+            res.json(receiver)
         }
         catch (err) {
             res.status(400).send(`Something went wrong! ${err}`)
