3934c38fe7733583b9956fdbc793dce207ea5dd4
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-03 19:18:20 +0530

    Removed password compare function because backend will not receive any passwords from the user from today as auth0 handles everything and removed duplicate functions because frontend handles the duplicates.

--- helper validation funcs/validations.js
+++ helper validation funcs/validations.js
@@ -1,14 +1,6 @@
 const { User } = require("../model/model");
 const { ConnectionRequest } = require("../model/connectRequest")
 
-const bcrypt = require("bcrypt");
-
-const encryptPassword = async (password) => {
-    const hashpw = await bcrypt.hash(password, 10);
-
-    return hashpw;
-}
-
 const valodationToNotAllowInvalidFields = (allowedFieldsForUpdation, req) => {
     const data = req.body;
     const keyOfData = Object.keys(data);
@@ -18,23 +10,4 @@ const valodationToNotAllowInvalidFields = (allowedFieldsForUpdation, req) => {
     return invalidKeys;
 }
 
-
-
-const checkDuplicationDuringUpdation = async (req) => {
-    const id = req.id;
-    const user = await User.findOne({ _id: id })
-
-    const headings = Object.keys(req.body);
-    const userOBJ = user.toObject();
-    const duplicated = headings.filter((heading) => userOBJ[heading] === req.body[heading]);
-
-    return duplicated;
-
-}
-
-
-
-
-
-
-module.exports = { encryptPassword, valodationToNotAllowInvalidFields, checkDuplicationDuringUpdation };
+module.exports = { valodationToNotAllowInvalidFields, checkDuplicationDuringUpdation };
