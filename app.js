96e9bb9ee9331450a62b72943c8ba338f32d5f4a
rinkal159 <rinkalsingapuri@gmail.com>
2025-08-19 16:14:28 +0530

    cookie-parser() and express.json() middlewares are defined in root file.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const connect = require("./config/database")
+const connect = require("./config/database");
 const updateMaritalField = require("./updateSchema/updateSchema.js")
+const cookieParser = require("cookie-parser");
 
 const dotenv = require("dotenv")
 dotenv.config(); // loads .env file into process.env
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
 const path = require("path");
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
-
 
 const cors = require("cors");
 app.use(cors({
@@ -17,33 +19,25 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
-
 const authRouter = require("./router/authRouter.js")
 const profileRouter = require("./router/profileRouter.js")
 const requestsRouter = require("./router/requestsRouter.js")
 const userRouter = require("./router/userRouter.js")
 
-{
-    // route handlers
-    app.use("/", authRouter);
-    app.use("/", profileRouter);
-    app.use("/", requestsRouter);
-    app.use("/", userRouter);
-}
+//* route handlers
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestsRouter);
+app.use("/", userRouter);
 
-
-{
-    // To update the schema
-    updateMaritalField()
-    
-}
+// To update the schema
+updateMaritalField();
 
 connect()
     .then(() => {
-        console.log("database success");
+        console.log("Database success");
         app.listen(process.env.PORT, () => {
-            console.log("server success");
+            console.log("Server success");
 
         })
     })
